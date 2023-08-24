3640641dfde1f7557c3bfd22191975105fcba3f2
Arne Limburg <arne.limburg@openknowledge.de>
2023-08-24 18:45:12 +0200

    feat: Add consumer driven contract testing

--- delivery-service/src/main/java/de/openknowledge/sample/address/domain/Address.java
+++ delivery-service/src/main/java/de/openknowledge/sample/address/domain/Address.java
@@ -15,18 +15,38 @@
  */
 package de.openknowledge.sample.address.domain;
 
-import static java.util.Optional.ofNullable;
 import static org.apache.commons.lang3.Validate.notNull;
-
-import java.util.Objects;
 
 import javax.json.bind.annotation.JsonbCreator;
 import javax.json.bind.annotation.JsonbProperty;
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "ADDRESSES")
 public class Address {
+
+    @EmbeddedId
+    CustomerNumber id;
+    @Embedded
     private Recipient recipient;
+    @Embedded
+    @AttributeOverrides({
+        @AttributeOverride(name = "name.name", column = @Column(name = "STREET")),
+        @AttributeOverride(name = "number.number", column = @Column(name = "HOUSE_NUMBER")),
+    })
     private Street street;
+    @Embedded
     private City city;
+
+    protected Address() {
+        // for JPA
+    }
 
     @JsonbCreator
     public Address(@JsonbProperty("recipient") Recipient recipient) {
@@ -57,50 +77,5 @@ public class Address {
 
     public void setCity(City city) {
         this.city = city;
-    }
-
-    @Override
-    public int hashCode() {
-        return recipient.hashCode() + ofNullable(street).map(Object::hashCode).orElse(0) + ofNullable(city).map(Object::hashCode).orElse(0);
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) {
-            return true;
-        }
-        if (object == null || !getClass().equals(object.getClass())) {
-            return false;
-        }
-        Address address = (Address)object;
-        return recipient.equals(recipient) && Objects.equals(street, address.street) && Objects.equals(city, address.city);
-    }
-
-    public static Builder of(String recipient) {
-        return new Builder(new Recipient(recipient));
-    }
-
-    public static class Builder {
-
-        private Address address;
-
-        private Builder(Recipient recipient) {
-            address = new Address(recipient);
-        }
-
-        public Builder atStreet(String name) {
-            AddressLine addressLine = new AddressLine(name);
-            address.setStreet(new Street(addressLine.getStreetName(), addressLine.getHouseNumber()));
-            return this;
-        }
-
-        public Builder inCity(String city) {
-            address.setCity(new City(city));
-            return this;
-        }
-
-        public Address build() {
-            return address;
-        }
     }
 }
